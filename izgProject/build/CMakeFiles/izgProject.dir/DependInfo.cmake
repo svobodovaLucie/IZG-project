2c0b13cfc740086b1edd42c557c21a50f71a8f06
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-14 20:55:47 +0200

    assignment added

---
+++ izgProject/build/CMakeFiles/izgProject.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucie/fit/izg/izgProject/examples/czFlagMethod.cpp" "CMakeFiles/izgProject.dir/examples/czFlagMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/czFlagMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/modelMethod.cpp" "CMakeFiles/izgProject.dir/examples/modelMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/modelMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/phongMethod.cpp" "CMakeFiles/izgProject.dir/examples/phongMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/phongMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/skFlagMethod.cpp" "CMakeFiles/izgProject.dir/examples/skFlagMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/skFlagMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/texturedQuadMethod.cpp" "CMakeFiles/izgProject.dir/examples/texturedQuadMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/texturedQuadMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/triangle3DMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangle3DMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangle3DMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/triangleBufferMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangleBufferMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleBufferMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/triangleClip1Method.cpp" "CMakeFiles/izgProject.dir/examples/triangleClip1Method.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleClip1Method.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/triangleClip2Method.cpp" "CMakeFiles/izgProject.dir/examples/triangleClip2Method.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleClip2Method.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/examples/triangleMethod.cpp" "CMakeFiles/izgProject.dir/examples/triangleMethod.cpp.o" "gcc" "CMakeFiles/izgProject.dir/examples/triangleMethod.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/application.cpp" "CMakeFiles/izgProject.dir/framework/application.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/application.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/bunny.cpp" "CMakeFiles/izgProject.dir/framework/bunny.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/bunny.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/main.cpp" "CMakeFiles/izgProject.dir/framework/main.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/main.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/model.cpp" "CMakeFiles/izgProject.dir/framework/model.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/model.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/textureData.cpp" "CMakeFiles/izgProject.dir/framework/textureData.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/textureData.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/framework/window.cpp" "CMakeFiles/izgProject.dir/framework/window.cpp.o" "gcc" "CMakeFiles/izgProject.dir/framework/window.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/libs/impl.cpp" "CMakeFiles/izgProject.dir/libs/impl.cpp.o" "gcc" "CMakeFiles/izgProject.dir/libs/impl.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/student/drawModel.cpp" "CMakeFiles/izgProject.dir/student/drawModel.cpp.o" "gcc" "CMakeFiles/izgProject.dir/student/drawModel.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/student/gpu.cpp" "CMakeFiles/izgProject.dir/student/gpu.cpp.o" "gcc" "CMakeFiles/izgProject.dir/student/gpu.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/clippingTests.cpp" "CMakeFiles/izgProject.dir/tests/clippingTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/clippingTests.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/conformanceTests.cpp" "CMakeFiles/izgProject.dir/tests/conformanceTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/conformanceTests.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/drawModelTests.cpp" "CMakeFiles/izgProject.dir/tests/drawModelTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/drawModelTests.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/finalImageTest.cpp" "CMakeFiles/izgProject.dir/tests/finalImageTest.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/finalImageTest.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/fragmentShaderTests.cpp" "CMakeFiles/izgProject.dir/tests/fragmentShaderTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/fragmentShaderTests.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/performanceTest.cpp" "CMakeFiles/izgProject.dir/tests/performanceTest.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/performanceTest.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/renderMethodFrame.cpp" "CMakeFiles/izgProject.dir/tests/renderMethodFrame.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/renderMethodFrame.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/saveFrame.cpp" "CMakeFiles/izgProject.dir/tests/saveFrame.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/saveFrame.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/takeScreenShot.cpp" "CMakeFiles/izgProject.dir/tests/takeScreenShot.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/takeScreenShot.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/testCommon.cpp" "CMakeFiles/izgProject.dir/tests/testCommon.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/testCommon.cpp.o.d"
+  "/home/lucie/fit/izg/izgProject/tests/vertexShaderTests.cpp" "CMakeFiles/izgProject.dir/tests/vertexShaderTests.cpp.o" "gcc" "CMakeFiles/izgProject.dir/tests/vertexShaderTests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lucie/fit/izg/izgProject/build/libs/SDL2-2.0.14/CMakeFiles/SDL2-static.dir/DependInfo.cmake"
+  "/home/lucie/fit/izg/izgProject/build/libs/SDL2-2.0.14/CMakeFiles/SDL2main.dir/DependInfo.cmake"
+  "/home/lucie/fit/izg/izgProject/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/DependInfo.cmake"
+  "/home/lucie/fit/izg/izgProject/build/libs/BasicCamera/CMakeFiles/BasicCamera.dir/DependInfo.cmake"
+  "/home/lucie/fit/izg/izgProject/build/libs/TxtUtils/CMakeFiles/TxtUtils.dir/DependInfo.cmake"
+  "/home/lucie/fit/izg/izgProject/build/libs/MealyMachine/CMakeFiles/MealyMachine.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
