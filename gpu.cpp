bec2188a40962ec5382dd707d133a8b6ecd5317e
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-26 07:07:49 +0200

    started second task (in comments)

--- gpu.cpp
+++ gpu.cpp
@@ -26,7 +26,6 @@ void computeVertexID(GPUContext &ctx, InVertex &inVertex, uint32_t i) {
 }
 
 void readAtributes(GPUContext &ctx, InVertex &inVertex) {
-    // readAttributes()
     for(uint32_t j = 0; j < maxAttributes; j++){
       auto const &currPos = ctx.vao.vertexAttrib[j];
       // pokud nejsou na dané pozici žádná data ke čtení, cyklus pokračuje dál
@@ -44,34 +43,87 @@ void readAtributes(GPUContext &ctx, InVertex &inVertex) {
     }
 }
 
-void runVertexAssembly(GPUContext &ctx, InVertex &inVertex, uint32_t i){
-  computeVertexID(ctx, inVertex, i);
+void runVertexAssembly(GPUContext &ctx, InVertex &inVertex, uint32_t v){
+  computeVertexID(ctx, inVertex, v);
   readAtributes(ctx, inVertex);
 }
-/*
+
 struct Triangle {
-  OutVertex points[3];
+  OutVertex point[3];
 };
 
-
-void loadTriangle(Triangle &triangle, GPUContext &ctx, InVertex inVertex, OutVertex outVertex, uint32_t tID, uint32_t nofVertices) {
-  
-  Triangle triangle;
-
-  for(unsigned i = 0; i < 3; i++) {
-    runVertexAssembly(ctx, nofVertices);
-    
-  }
-}
-
-
-void runPrimitiveAssembly(GPUContext &ctx, uint32_t nofVertices) {
+void runPrimitiveAssembly(Triangle &triangle, GPUContext &ctx, uint32_t t) {
 
   // vrchol co vyleze z vertex shaderu (x, y, z, w)
-  runVertexAssembly(ctx, nofVertices);
+  for(uint32_t v = 0; v < 3; v++){
+    InVertex inVertex;
+    runVertexAssembly(ctx, inVertex, t + v);
+    ctx.prg.vertexShader(triangle.point[v], inVertex, ctx.prg.uniforms);
+  }
+}
+/*
+float area(glm::vec2 const&A,glm::vec2 const&B,glm::vec2 const&C){
+  auto a=glm::length(B-A);
+  auto b=glm::length(C-B);
+  auto c=glm::length(A-C);
+  auto s = (a+b+c)/2.f;
+  return glm::sqrt(s*glm::abs(s-a)*glm::abs(s-b)*glm::abs(s-c));
+}
+
+glm::vec3 runPerspectiveDivision(Triangle &triangle, uint32_t width,uint32_t height,glm::vec2 x) {
+  // clip-space
+  glm::vec4 clip[3] = {
+    triangle.point[0].gl_Position,
+    triangle.point[1].gl_Position,
+    triangle.point[2].gl_Position
+  };
+
+  // perspektivní dělení
+  glm::vec3 ndc[3];
+  for(int i=0;i<3;++i)
+    ndc[i] = glm::vec3(clip[i])/clip[i].w;
+  
+  // viewport transformace
+  glm::vec3 wc[3];
+  for(int i=0;i<3;++i)
+    wc[i] = glm::vec3((glm::vec2(ndc[i])*.5f+.5f)*glm::vec2(width, height),ndc[i].z);
+  */
+  /*
+  float T = area(wc[0],wc[1],wc[2]);
+  glm::vec3 l;
+  for(int i=0;i<3;++i)l[i] = area(wc[(i+1)%3],wc[(i+2)%3],x)/T;
+  return l;
+  */
+  //}
+
+/*
+  float T = area(wc[0],wc[1],wc[2]);
+  glm::vec3 l;
+  for(int i=0;i<3;++i)
+    l[i] = area(wc[(i+1)%3],wc[(i+2)%3],x)/T;
+  return l;
+}
+
+glm::vec3 barycentricsPerspective(Triangle &triangle,glm::vec2 x,uint32_t w,uint32_t h, glm::vec2 x){
+  auto l = runPerspectiveDivision(&triangle,width,height, x);
+  //glm::vec4 clip[3]={a.gl_Position,b.gl_Position,c.gl_Position};
+  glm::vec4 clip[3] = {
+    triangle.point[0].gl_Position,
+    triangle.point[1].gl_Position,
+    triangle.point[2].gl_Position
+  };
+  for(int i=0;i<3;++i)l[i] /= clip[i].w;
+  l /= glm::dot(l,glm::vec3(1.f));
+  return l;
+}
+
+void runViewportTransformation(Triangle &triangle, GPUContext &ctx) {
 
 }
 
+void rasterizeTriangle(Triangle &triangle, GPUContext &ctx) {
+
+}
 */
 //! [drawTrianglesImpl]
 void drawTrianglesImpl(GPUContext &ctx,uint32_t nofVertices){
@@ -91,17 +143,113 @@ void drawTrianglesImpl(GPUContext &ctx,uint32_t nofVertices){
   VertexArray const &vao = ctx.vao;   // vertex array
 
   // for every vertex
-  for(uint32_t i = 0; i < nofVertices; i++){
-    InVertex inVertex;
-    OutVertex outVertex;
-  
-    runVertexAssembly(ctx, inVertex, i);
 
+  /*************** 2. Úkol - naprogramovat Primitive Assembly jednotku, rasterizaci a pouštění fragment shaderu ***************/
+  
+  for(u_int32_t t = 0; t < nofVertices; t += 3) {
+    Triangle triangle;
+    runPrimitiveAssembly(triangle, ctx, t);
+    //runPerspectiveDivision(triangle, ctx.frame.width, ctx.frame.height);
+  }
+}
+    //auto const&f:inFragments;
+    //barycentricsPerspective(triangle, ctx.frame.width, ctx.frame.height, f.gl_FragCoord);
+/*//////////////////////
+float area(glm::vec2 const&A,glm::vec2 const&B,glm::vec2 const&C){
+  auto a=glm::length(B-A);
+  auto b=glm::length(C-B);
+  auto c=glm::length(A-C);
+  auto s = (a+b+c)/2.f;
+  return glm::sqrt(s*glm::abs(s-a)*glm::abs(s-b)*glm::abs(s-c));
+}
+
+  glm::vec4 clip[3]={a.gl_Position,b.gl_Position,c.gl_Position};
+  glm::vec3 ndc[3];
+  for(int i=0;i<3;++i)ndc[i] = glm::vec3(clip[i])/clip[i].w;
+  glm::vec3 wc[3];
+  for(int i=0;i<3;++i)wc[i] = glm::vec3((glm::vec2(ndc[i])*.5f+.5f)*glm::vec2(w,h),ndc[i].z);
+  float T = area(wc[0],wc[1],wc[2]);
+  glm::vec3 l;
+  for(int i=0;i<3;++i)l[i] = area(wc[(i+1)%3],wc[(i+2)%3],x)/T;
+
+glm::vec3 barycentricsPerspective(OutVertex const&a,OutVertex const&b,OutVertex const&c,glm::vec2 x,uint32_t w,uint32_t h){
+  auto l = barycentrics(a,b,c,x,w,h);
+  glm::vec4 clip[3]={a.gl_Position,b.gl_Position,c.gl_Position};
+  for(int i=0;i<3;++i)l[i] /= clip[i].w;
+  l /= glm::dot(l,glm::vec3(1.f));
+  return l;
+}
+*/ ////////////////////////////////
+    //runViewportTransformation(triangle, ctx);
+
+
+    //rasterizeTriangle(triangle, ctx);
+    /*
+    for(u_int32_t y = 0; y < ctx.frame.width; y++) {
+      for(u_int32_t x = 0; x < ctx.frame.height; x++) {
+        if () {
+          
+        }
+      }
+    }
+    
+  }
+  */
+ /************** GOOD
+  for(u_int32_t t = 0; t < nofVertices; t++) {
+    //runPrimitiveAssembly(triangle, ctx, t);
+    InVertex inVertex;
+    //runVertexAssembly(ctx, inVertex, t + v);
+    //computeVertexID(ctx, inVertex, v);
+    // kontrola, zda je zapnuté indexování
+    if(ctx.vao.indexBuffer){  // vao.indexBuffer =! nullptr -> indexování je zapnuto
+      // 32-bitové indexování
+      if(ctx.vao.indexType == IndexType::UINT32)
+        inVertex.gl_VertexID = ((uint32_t*)ctx.vao.indexBuffer)[t];
+      // 16-bitové indexování
+      else if(ctx.vao.indexType == IndexType::UINT16)
+        inVertex.gl_VertexID = ((uint16_t*)ctx.vao.indexBuffer)[t];
+      // 8-bitové indexování
+      else if(ctx.vao.indexType == IndexType::UINT8 )
+        inVertex.gl_VertexID = ((uint8_t *)ctx.vao.indexBuffer)[t];
+
+    }else{  // vao.indexBuffer == nullptr -> indexování není zapnuto
+      inVertex.gl_VertexID = t;
+    }
+
+    //readAtributes(ctx, inVertex);    
+    for(uint32_t j = 0; j < maxAttributes; j++){
+      auto const &currPos = ctx.vao.vertexAttrib[j];
+      // pokud nejsou na dané pozici žádná data ke čtení, cyklus pokračuje dál
+      if(!currPos.bufferData)
+        continue;
+      // pokud jsou na dané pozici nějaká data, zjistíme jaká a přečteme je z adresy buf_ptr + offset + stride*gl_VertexID
+      if(currPos.type == AttributeType::FLOAT)
+        inVertex.attributes[j].v1 = *(float*)(((uint8_t*)currPos.bufferData) + currPos.offset + currPos.stride * inVertex.gl_VertexID);
+      if(currPos.type == AttributeType::VEC2 )
+        inVertex.attributes[j].v2 = *(glm::vec2*)(((uint8_t*)currPos.bufferData) + currPos.offset + currPos.stride * inVertex.gl_VertexID);
+      if(currPos.type == AttributeType::VEC3 )
+        inVertex.attributes[j].v3 = *(glm::vec3*)(((uint8_t*)currPos.bufferData) + currPos.offset + currPos.stride * inVertex.gl_VertexID);
+      if(currPos.type == AttributeType::VEC4 )
+        inVertex.attributes[j].v4 = *(glm::vec4*)(((uint8_t*)currPos.bufferData) + currPos.offset + currPos.stride * inVertex.gl_VertexID);
+    }
+    OutVertex outVertex;
     ctx.prg.vertexShader(outVertex, inVertex, ctx.prg.uniforms);
   }
-  /*************** 2. Úkol - naprogramovat Primitive Assembly jednotku, rasterizaci a pouštění fragment shaderu ***************/
+}
+*/////////////////// GOOD END
 
-    //runPrimitiveAssembly(ctx, nofVertices);
+
+    /**************/
+    //InVertex inVertex;
+    //runVertexAssembly(ctx, inVertex, t);
+    //OutVertex outVertex;
+    //ctx.prg.vertexShader(outVertex, inVertex, ctx.prg.uniforms);
+    
+    //runPerspectiveDivision(triangle, 100, 100);
+    //runViewportTransformation(triangle, ctx);
+    //rasterizeTriangle(triangle, ctx);
+  
   
 /****
 
@@ -137,7 +285,7 @@ void drawTriangles(GPUContext&ctx,uint32_t nofVertices){
  */
 
 
-}
+
 //! [drawTrianglesImpl]
 
 /**
